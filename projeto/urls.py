a9347ad61e42e15e7b8f1d649f1d459e6411f872
Ferrúcio Lima Melo <flmelo19@gmail.com>
2023-11-25 22:44:34 -0300

    recipes app

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
+from django.urls import path, include
 
 
-def my_view(request):
-    return HttpResponse('Página inicial <3')
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', my_view),
+    path('', include('recipes.urls')),
+
 ]
